f4225c6e13367d4e7e71d974020a725248e385a1
RinatOdnogulov <rinatodnogulov85@gmail.com>
2023-10-05 16:21:58 +0300

    last commit

--- 02/search_parse_json.py
+++ 02/search_parse_json.py
@@ -10,7 +10,7 @@ def parse_json(json_str: str, keyword_callback, required_fields=None,  keywords=
     count = 0
     for key in json_doc.keys():
         if key in required_fields:
-            for word in json_doc[key].split():
+            for word in json_doc[key].lower().split():
                 if word in keywords:
                     words_callback(word)
 
