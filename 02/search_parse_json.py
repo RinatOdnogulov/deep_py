21af90827cc5977567ee0f2dfd2e396e60fd83b5
RinatOdnogulov <rinatodnogulov85@gmail.com>
2023-10-06 11:49:58 +0300

    last commit

--- 02/search_parse_json.py
+++ 02/search_parse_json.py
@@ -6,14 +6,19 @@ def words_callback(word):
 
 
 def parse_json(json_str: str, keyword_callback, required_fields=None,  keywords=None):
+    print(json_str)
     json_doc = json.loads(json_str)
-    count = 0
+    print(json_doc)
     for key in json_doc.keys():
         if key in required_fields:
             for word in json_doc[key].lower().split():
                 if word in keywords:
-                    words_callback(word)
+                    keyword_callback(word)
 
 
+json_str = '{"key1": "Word1 Word2 word2", "key2": "word2 word3"}'
+required_fields = ["key1"]
+keywords = ["word2"]
 
 
+parse_json(json_str, words_callback, required_fields, keywords)
